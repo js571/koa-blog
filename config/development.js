6fb3d1518c8e1cea9a203375d0298fbf161db6be
maat <maat@ixiaopu.com>
2016-11-28 16:42:05 +0800

    完善配置

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   port: process.env.PORT || 3000,
-  name: 'N-club',
+  name: 'koa_blog',
   mongodb: {
     url: 'mongodb://127.0.0.1:27017/koa_blog'
   },
